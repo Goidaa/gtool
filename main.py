f8a90dab154175f8db53ffda7e324c1c04bf58ca
Gondy <157113135+Goidaa@users.noreply.github.com>
2024-10-23 21:06:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import time
 import random
 from telegraph import Telegraph
+from colorama import Fore, Style, init
+
+# Инициализация Colorama
+init(autoreset=True)
 
 def generate_glitch_text(length):
     glitch_chars = ['�', '▒', '▓', '░', '█', '▌', '▐', '▖', '▗', '▘', '▝', '▞', '▟', '▃', '▄', '▅', '▆', '▇']
     return ''.join(random.choice(glitch_chars) for _ in range(length))
+
 print(f'https://github.com/Goidaa/telegraph-flooder/')
 author_name = input("Enter author name: ")
 custom_text = input("Enter text: ")
 page_title = input("Enter page title: ")
 
-account_names = ['RC1', 'RC2', 'RC3']
+account_names = ['GFL', 'GFL_GOOON', 'GFL_GONNDYYYY']
 accounts = []
 for account_name in account_names:
     account = Telegraph()
     account.create_account(short_name=account_name)
     accounts.append(account)
+    print(Fore.MAGENTA + f'Created new account: {account_name}')  
 
 i = 1
 while True:
@@ -24,7 +30,7 @@ while True:
         new_account = Telegraph()
         new_account.create_account(short_name=new_account_name)
         accounts.append(new_account)
-        print(f'Created new account: {new_account_name}')
+        print(Fore.MAGENTA + f'Created new account: {new_account_name}')  
 
     account = accounts[i % len(accounts)]
     title = f'{page_title} {i}'
@@ -38,10 +44,10 @@ while True:
             author_name=author_name,
             author_url="https://dsc.gg/rcteam"
         )
-        print(f'Created page {i}: https://telegra.ph/' + response['path'])
+        print(Fore.LIGHTGREEN_EX + f'Created page {i}: https://telegra.ph/' + response['path'])  
         
     except Exception as e:
-        print(f'Error for account {account_names[i % len(accounts)]}: {e}')
+        print(Fore.RED + f'Error for account {account_names[i % len(accounts)]}: {e}')  
     
     i += 1
     time.sleep(0.0001)
